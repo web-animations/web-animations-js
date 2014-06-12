283d3c6a056cd2612b6ec62a067c097bda083f46
Alan Cutter <alancutter@chromium.org>
2014-06-12 16:43:18 +1000

    Avoid stashing for simplicity and size

--- src/apply.js
+++ src/apply.js
@@ -14,24 +14,12 @@
 
 (function(scope, testing) {
 
-  function stashName(property) {
-    return '_' + property;
-  }
-
   scope.apply = function(element, property, value) {
-    var stash = stashName(property);
-    if (!element.style.hasOwnProperty(stash)) {
-      element.style[stash] = element.style[property];
-    }
     element.style[property] = value;
   };
 
   scope.clear = function(element, property) {
-    var stash = stashName(property);
-    if (element.style.hasOwnProperty(stash)) {
-      element.style[property] = element.style[stash];
-      delete element.style[stash];
-    }
+    element.style[property] = '';
   };
 
 })(webAnimations, testing);
