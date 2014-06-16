ac77fe11fff00c0670f7a0953c8d91b56528362d
Alan Cutter <alancutter@chromium.org>
2014-06-16 15:15:45 +1000

    Update apply-preserving-inline-style-methods test

--- test/js/apply-preserving-inline-style-methods.js
+++ test/js/apply-preserving-inline-style-methods.js
@@ -13,32 +13,15 @@ suite('apply-preserving', function() {
     assert(this.element._webAnimationsPatchedStyle);
   });
   test('Setting animated style', function() {
-    this.style.left = '0px';
+    this.style.setProperty('left', '0px');
     this.element.style._set('left', '100px');
-    assert.equal(this.style.left, '0px');
+    assert.equal(this.style.getPropertyValue('left'), '0px');
   });
   test('Clearing animated style', function() {
-    this.style.left = '0px';
+    this.style.setProperty('left', '0px');
     this.element.style._set('left', '100px');
     this.element.style._clear('left');
-    assert.equal(this.style.left, '0px');
-  });
-  test('Patched length', function() {
-    this.element.style._set('left', '100px');
-    this.style.cssText = 'left: 0px; background-color: green;';
-    assert.equal(this.style.cssText, 'left: 0px; background-color: green;');
-    assert.equal(this.style.left, '0px');
-    assert.equal(this.style.backgroundColor, 'green');
-    assert.equal(this.style.length, 2);
-  });
-  test('Patched property getters and setters', function() {
-    this.style._set('left', '100px');
-    this.style.left = '0px';
-    this.style.backgroundColor = 'rgb(1, 2, 3)';
-    assert.equal(this.style.left, '0px');
-    assert.equal(this.style.backgroundColor, 'rgb(1, 2, 3)');
-    assert.equal(getComputedStyle(this.element).left, '100px');
-    assert.equal(getComputedStyle(this.element).backgroundColor, 'rgb(1, 2, 3)');
+    assert.equal(this.style.getPropertyValue('left'), '0px');
   });
   test('Patched setProperty/getPropertyValue', function() {
     this.style._set('left', '100px');
@@ -51,14 +34,11 @@ suite('apply-preserving', function() {
   });
   test('Patched item()', function() {
     this.style._set('left', '100px');
-    this.style.setProperty('left', '0px');
+    this.style.setProperty('top', '0px');
     this.style.setProperty('background-color', 'rgb(1, 2, 3)');
-    assert.equal(this.style.item(0), 'left');
+    assert.equal(this.style.item(0), 'top');
     assert.equal(this.style.item(1), 'background-color');
     assert.equal(this.style.item(2), '');
-    this.style.cssText = 'top: 0px';
-    assert.equal(this.style.item(0), 'top');
-    assert.equal(this.style.item(1), '');
   });
   test('Patched cssText', function() {
     this.style._set('left', '100px');
