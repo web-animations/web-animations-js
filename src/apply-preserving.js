ce7c0da0eb4fa4d4968fbfe27efd2a1d2b82ef74
Alan Cutter <alancutter@chromium.org>
2014-06-16 11:21:58 +1000

    Import the full CSSStyleDeclaration patch

--- src/apply-preserving.js
+++ src/apply-preserving.js
@@ -14,21 +14,151 @@
 
 (function(scope, testing) {
 
-  var copyInlineStyle = function(sourceStyle, destinationStyle) {
+  var styleAttributes = {
+    cssText: 1,
+    length: 1,
+    parentRule: 1,
+  };
+
+  var styleMethods = {
+    getPropertyCSSValue: 1,
+    getPropertyPriority: 1,
+    getPropertyValue: 1,
+    item: 1,
+    removeProperty: 1,
+    setProperty: 1,
+  };
+
+  var styleMutatingMethods = {
+    removeProperty: 1,
+    setProperty: 1,
+  };
+
+  function configureDescriptor(descriptor) {
+    descriptor.enumerable = true;
+    descriptor.configurable = true;
+    return descriptor;
+  }
+
+  function configureProperty(object, property, descriptor) {
+    Object.defineProperty(object, property, configureDescriptor(descriptor));
+  }
+
+  function copyInlineStyle(sourceStyle, destinationStyle) {
     for (var i = 0; i < sourceStyle.length; i++) {
       var property = sourceStyle[i];
       destinationStyle[property] = sourceStyle[property];
     }
   };
 
-  var styleMethods = (
-    'getPropertyCSSValue|' +
-    'getPropertyPriority|' +
-    'getPropertyValue|' +
-    'item|' +
-    'removeProperty|' +
-    'setProperty'
-  ).split('|');
+  function AnimatedCSSStyleDeclaration(element) {
+    TESTING && console.assert(!(element.style instanceof AnimatedCSSStyleDeclaration),
+        'Element must not already have an animated style attached.');
+
+    // Stores the inline style of the element on its behalf while the
+    // polyfill uses the element's inline style to simulate web animations.
+    // This is needed to fake regular inline style CSSOM access on the element.
+    this._surrogateElement = document.createElement('div');
+    this._surrogateStyle = this._surrogateElement.style;
+    this._style = element.style;
+    this._length = 0;
+    this._isAnimatedProperty = {};
+
+    copyInlineStyle(this._style, this._surrogateStyle);
+    this._updateIndices();
+  };
+
+  AnimatedCSSStyleDeclaration.prototype = {
+    get cssText() {
+      return this._surrogateStyle.cssText;
+    },
+    set cssText(text) {
+      var isAffectedProperty = {};
+      for (var i = 0; i < this._surrogateStyle.length; i++) {
+        isAffectedProperty[this._surrogateStyle[i]] = true;
+      }
+      this._surrogateStyle.cssText = text;
+      this._updateIndices();
+      for (var i = 0; i < this._surrogateStyle.length; i++) {
+        isAffectedProperty[this._surrogateStyle[i]] = true;
+      }
+      for (var property in isAffectedProperty) {
+        if (!this._isAnimatedProperty[property]) {
+          this._style.setProperty(property, this._surrogateStyle.getPropertyValue(property));
+        }
+      }
+    },
+    get length() {
+      return this._surrogateStyle.length;
+    },
+    get parentRule() {
+      return this._style.parentRule;
+    },
+    // Mirror the indexed getters and setters of the surrogate style.
+    _updateIndices: function() {
+      while (this._length < this._surrogateStyle.length) {
+        Object.defineProperty(this, this._length, {
+          configurable: true,
+          enumerable: false,
+          get: (function(index) {
+            return function() { return this._surrogateStyle[index]; };
+          })(this._length)
+        });
+        this._length++;
+      }
+      while (this._length > this._surrogateStyle.length) {
+        this._length--;
+        Object.defineProperty(this, this._length, {
+          configurable: true,
+          enumerable: false,
+          value: undefined
+        });
+      }
+    },
+    _set: function(property, value) {
+      this._style[property] = value;
+      this._isAnimatedProperty[property] = true;
+    },
+    _clear: function(property) {
+      this._style[property] = this._surrogateStyle[property];
+      delete this._isAnimatedProperty[property];
+    },
+  };
+
+  // Wrap the style methods.
+  for (var method in styleMethods) {
+    AnimatedCSSStyleDeclaration.prototype[method] = (function(method, modifiesStyle) {
+      return function() {
+        var result = this._surrogateStyle[method].apply(this._surrogateStyle, arguments);
+        if (modifiesStyle) {
+          if (!this._isAnimatedProperty[arguments[0]])
+            this._style[method].apply(this._style, arguments);
+          this._updateIndices();
+        }
+        return result;
+      }
+    })(method, method in styleMutatingMethods);
+  }
+
+  // Wrap the style.cssProperty getters and setters.
+  for (var property in document.documentElement.style) {
+    if (property in styleAttributes || property in styleMethods) {
+      continue;
+    }
+    (function(property) {
+      configureProperty(AnimatedCSSStyleDeclaration.prototype, property, {
+        get: function() {
+          return this._surrogateStyle[property];
+        },
+        set: function(value) {
+          this._surrogateStyle[property] = value;
+          this._updateIndices();
+          if (!this._isAnimatedProperty[property])
+            this._style[property] = value;
+        }
+      });
+    })(property);
+  }
 
   // This patching method is a fallback for when we are unable to replace the browser's element.style property.
   // This has the disadvantage that reading style.cssProperty getters will return the animated value.
@@ -39,24 +169,20 @@
     var surrogateElement = document.createElement('div');
     copyInlineStyle(style, surrogateElement.style);
     var isAnimatedProperty = {};
-    styleMethods.forEach(function(methodName) {
-      if (!(methodName in style)) {
+    for (var method in styleMethods) {
+      if (!(method in style))
         return;
-      }
-      Object.defineProperty(style, methodName, {
-        configurable: true,
-        enumerable: true,
-        value: (function(methodName, originalMethod) {
+      configureProperty(style, method, {
+        value: (function(method, originalMethodFunction) {
           return function(property) {
-            var result = surrogateElement.style[methodName].apply(surrogateElement.style, arguments);
-            if (!isAnimatedProperty[property]) {
-              originalMethod.apply(style, arguments);
-            }
+            var result = surrogateElement.style[method].apply(surrogateElement.style, arguments);
+            if (!isAnimatedProperty[property])
+              originalMethodFunction.apply(style, arguments);
             return result;
           }
-        })(methodName, style[methodName]),
+        })(method, style[method]),
       });
-    });
+    }
 
     style._set = function(property, value) {
       style[property] = value;
@@ -67,16 +193,19 @@
       style[property] = surrogateElement.style[property];
       delete isAnimatedProperty[property];
     };
-
-    // We must keep a handle on the patched style to prevent it from getting GC'd.
-    element._webAnimationsPatchedStyle = element.style;
   }
 
   function ensureStyleIsPatched(element) {
-    if (element._webAnimationsPatchedStyle) {
+    if (element._webAnimationsPatchedStyle)
       return;
+    try {
+      var animatedStyle = new AnimatedCSSStyleDeclaration(element);
+      configureProperty(element, 'style', { get: function() { return animatedStyle; } });
+    } catch (error) {
+      patchInlineStyleForAnimation(element.style);
     }
-    patchInlineStyleForAnimation(element);
+    // We must keep a handle on the patched style to prevent it from getting GC'd.
+    element._webAnimationsPatchedStyle = element.style;
   }
 
   scope.apply = function(element, property, value) {
