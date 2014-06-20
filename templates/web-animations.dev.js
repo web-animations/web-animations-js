c8f28351d0e8375819c55a5b2343f7a0b22c238a
Alan Cutter <alancutter@chromium.org>
2014-06-20 18:04:50 +1000

    Fix bad src path

--- templates/web-animations.dev.js
+++ templates/web-animations.dev.js
@@ -1,5 +1,5 @@
 var global = this;
-if (this.TESTING === undefined) {
+if (typeof this.TESTING === 'undefined') {
   var TESTING = false;
 }
 
@@ -15,6 +15,6 @@ if (this.TESTING === undefined) {
   var scripts = document.getElementsByTagName('script');
   var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
   sources.forEach(function(src) {
-    document.write('<script src="' + location + src + '"></script>');
+    document.write('<script src="' + location + 'src/' + src + '"></script>');
   });
 })();
