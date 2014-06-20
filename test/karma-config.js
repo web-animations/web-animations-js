50cf829da718f9db18fa42a82ffcc89e956e0f44
Alan Cutter <alancutter@chromium.org>
2014-06-20 21:16:55 +1000

    Add grunt test command to launch karma testing

---
+++ test/karma-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  frameworks: ['mocha', 'chai'],
+  plugins: [
+    'karma-mocha',
+    'karma-chai',
+    'karma-chrome-launcher'
+    // 'karma-firefox-launcher'
+  ],
+  browsers: ['Chrome'],
+  // browsers: ['Safari', 'Chrome', 'ChromeCanary', 'Firefox', 'IE'],
+  basePath: '.',
+  files: [
+    // Populated in `grunt test` task.
+  ],
+  singleRun: true,
+  port: 9876,
+  reporters: ['progress'],
+  colors: true,
+  autoWatch: false,
+};
