dff456db51439ae4fcd1308ec2aadade72ff0f82
Alan Cutter <alancutter@chromium.org>
2014-06-20 19:19:51 +1000

    Add target-config comment

--- target-config.js
+++ target-config.js
@@ -53,7 +53,8 @@
     'transform-handler.js',
   ];
 
-  var config = {
+  // This object specifies the source and test files for different Web Animation build targets.
+  var targetConfig = {
     minifill: {
       src: minifillSrc,
       test: minifillTest,
@@ -87,7 +88,7 @@
   };
 
   if (typeof module != 'undefined')
-    module.exports = config;
+    module.exports = targetConfig;
   else
-    window.targetConfig = config;
+    window.targetConfig = targetConfig;
 })();
