45200e20ef3e1b23b9b8dd00fe67a2e1833158d9
Alan Cutter <alancutter@chromium.org>
2014-06-21 18:09:17 +1000

    Add pre-generated minifill files

---
+++ web-animations.js
@@ -0,0 +1,17 @@
+var global = this;
+var TESTING = false;
+
+(function() {
+  function lastScriptElement() {
+    var scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1];
+  }
+
+  var location = lastScriptElement().src.replace(/[^\/]+$/, '');
+  document.write('<script src="' + location + 'target-config.js"></script>');
+  lastScriptElement().addEventListener('load', function() {
+    targetConfig['minifill'].src.forEach(function(sourceFile) {
+      document.write('<script src="' + location + sourceFile + '"></script>');
+    });
+  });
+})();
