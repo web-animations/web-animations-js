ac21556895341da540e0e4840f4bca3dabe46cfb
Alan Cutter <alancutter@chromium.org>
2014-06-23 15:21:42 +1000

    Make web-animations*.dev.js work in Firefox

--- web-animations.js
+++ web-animations.js
@@ -1,17 +1,10 @@
 var global = this;
 var TESTING = false;
+var webAnimationsSourceTarget = null;
 
 (function() {
-  function lastScriptElement() {
-    var scripts = document.getElementsByTagName('script');
-    return scripts[scripts.length - 1];
-  }
-
-  var location = lastScriptElement().src.replace(/[^\/]+$/, '');
+  var scripts = document.getElementsByTagName('script');
+  var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
   document.write('<script src="' + location + 'target-config.js"></script>');
-  lastScriptElement().addEventListener('load', function() {
-    targetConfig[targetConfig.defaultTarget].src.forEach(function(sourceFile) {
-      document.write('<script src="' + location + sourceFile + '"></script>');
-    });
-  });
+  document.write('<script src="' + location + 'target-loader.js"></script>');
 })();
