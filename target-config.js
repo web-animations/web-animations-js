14a8a3d2161e823ae6de8dd896d4decb7d969b37
Douglas Stockwell <dstockwell@chromium.org>
2014-06-26 14:27:18 +1000

    Organise source files by target

--- target-config.js
+++ target-config.js
@@ -1,92 +1,62 @@
 (function() {
 
-  function tweak(files, changes) {
-    var newFiles = files.slice();
-    if (changes.replace) {
-      for (file in changes.replace) {
-        var index = newFiles.indexOf(file);
-        if (index == -1)
-          throw file + ' not found when replacing ' + file + ' with ' + changes.replace[file];
-        newFiles[index] = changes.replace[file];
-      }
-    }
-    if (changes.remove) {
-      changes.remove.forEach(function(file) {
-        var index = newFiles.indexOf(file);
-        if (index >= 0)
-          newFiles.splice(index, 1);
-      });
-    }
-    if (changes.add)
-      newFiles = newFiles.concat(changes.add);
-    if (changes.addTop)
-      newFiles = changes.addTop.concat(newFiles);
-    return newFiles;
-  }
+  var src = [
+      'src/scope.js', // Must be first.
+      'src/shared/animation-node.js',
+      'src/shared/effect.js'];
 
-  var minifillSrc = [
-    'src/scope.js',
-    'src/animation-node.js',
-    'src/animation.js',
-    'src/apply.js',
-    'src/property-interpolation.js',
-    'src/color-handler.js',
-    'src/dimension-handler.js',
-    'src/effect.js',
-    'src/element-animate.js',
-    'src/interpolation.js',
-    'src/number-handler.js',
-    'src/player.js',
-    'src/timeline.js',
-    'src/transform-handler.js',
-  ];
+  var minifillSrc = src.concat(
+      // FIXME: Currently property-interpolation must come before handlers.
+      'src/minifill/property-interpolation.js',
+      'src/minifill/animation.js',
+      'src/minifill/apply.js',
+      'src/minifill/color-handler.js',
+      'src/minifill/dimension-handler.js',
+      'src/minifill/element-animate.js',
+      'src/minifill/interpolation.js',
+      'src/minifill/number-handler.js',
+      'src/minifill/player.js',
+      'src/minifill/timeline.js',
+      'src/minifill/transform-handler.js');
+
+  var maxifillSrc = minifillSrc.concat(
+      'src/maxifill/animation-constructor.js',
+      'src/maxifill/effect-callback.js',
+      'src/maxifill/group-constructors.js');
 
   var minifillTest = [
-    'test/js/animation-node.js',
-    'test/js/color-handler.js',
-    'test/js/dimension-handler.js',
-    'test/js/effect.js',
-    'test/js/element-animate.js',
-    'test/js/interpolation.js',
-    'test/js/number-handler.js',
-    'test/js/player-finish-event.js',
-    'test/js/player.js',
-    'test/js/property-interpolation.js',
-    'test/js/transform-handler.js',
-    'test/js/timeline.js'
-  ];
+      'test/js/animation-node.js',
+      'test/js/color-handler.js',
+      'test/js/dimension-handler.js',
+      'test/js/effect.js',
+      'test/js/element-animate.js',
+      'test/js/interpolation.js',
+      'test/js/number-handler.js',
+      'test/js/player-finish-event.js',
+      'test/js/player.js',
+      'test/js/property-interpolation.js',
+      'test/js/transform-handler.js',
+      'test/js/timeline.js'];
+
+  var maxifillTest = minifillTest.concat(
+      'test/js/group-constructors.js',
+      'test/js/group-player.js',
+      'test/js/group-player-finish-event.js',
+      'test/js/effect-callback.js');
 
   // This object specifies the source and test files for different Web Animation build targets.
   var targetConfig = {
     minifill: {
       src: minifillSrc,
       test: minifillTest,
-      customSuffix: '',
     },
-
     maxifill: {
-      src: tweak(minifillSrc, {
-        add: [
-          'src/animation-constructor.js',
-          'src/group-constructors.js',
-          'src/effect-callback.js',
-        ],
-      }),
-      test: tweak(minifillTest, {
-        addTop: [
-          'test/js/group-constructors.js',
-          'test/js/group-player.js',
-          'test/js/group-player-finish-event.js',
-          'test/js/effect-callback.js',
-        ],
-      }),
+      src: maxifillSrc,
+      test: maxifillTest,
     },
   };
 
-  // The default target will be used for files without target suffixes:
-  // - test/runner.html
-  // - web-animations.js
-  // - web-animations.min.js
+  // The default target will be used for files without target suffixes, eg. web-animations.js
   Object.defineProperty(targetConfig, 'defaultTarget', {
     configurable: true,
     enumerable: false,
