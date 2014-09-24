ea8d973bf666d6d43fb103b242ac95c5ef62651b
rjwright <rjwright@chromium.org>
2014-09-24 17:35:31 +1000

    Moved matrix code into its own module

--- src/interpolation.js
+++ src/interpolation.js
@@ -13,170 +13,6 @@
 // limitations under the License.
 
 (function(scope, testing) {
-  var composeMatrix = (function() {
-    function multiply(a, b) {
-      var result = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-      for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
-          for (var k = 0; k < 4; k++) {
-            result[i][j] += b[i][k] * a[k][j];
-          }
-        }
-      }
-      return result;
-    }
-
-    function is2D(m) {
-      return (
-          m[0][2] == 0 &&
-          m[0][3] == 0 &&
-          m[1][2] == 0 &&
-          m[1][3] == 0 &&
-          m[2][0] == 0 &&
-          m[2][1] == 0 &&
-          m[2][2] == 1 &&
-          m[2][3] == 0 &&
-          m[3][2] == 0 &&
-          m[3][3] == 1);
-    }
-
-    function composeMatrix(translate, scale, skew, quat, perspective) {
-      var matrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
-
-      for (var i = 0; i < 4; i++) {
-        matrix[i][3] = perspective[i];
-      }
-
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          matrix[3][i] += translate[j] * matrix[j][i];
-        }
-      }
-
-      var x = quat[0], y = quat[1], z = quat[2], w = quat[3];
-
-      var rotMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
-
-      rotMatrix[0][0] = 1 - 2 * (y * y + z * z);
-      rotMatrix[0][1] = 2 * (x * y - z * w);
-      rotMatrix[0][2] = 2 * (x * z + y * w);
-      rotMatrix[1][0] = 2 * (x * y + z * w);
-      rotMatrix[1][1] = 1 - 2 * (x * x + z * z);
-      rotMatrix[1][2] = 2 * (y * z - x * w);
-      rotMatrix[2][0] = 2 * (x * z - y * w);
-      rotMatrix[2][1] = 2 * (y * z + x * w);
-      rotMatrix[2][2] = 1 - 2 * (x * x + y * y);
-
-      matrix = multiply(matrix, rotMatrix);
-
-      var temp = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
-      if (skew[2]) {
-        temp[2][1] = skew[2];
-        matrix = multiply(matrix, temp);
-      }
-
-      if (skew[1]) {
-        temp[2][1] = 0;
-        temp[2][0] = skew[0];
-        matrix = multiply(matrix, temp);
-      }
-
-      if (skew[0]) {
-        temp[2][0] = 0;
-        temp[1][0] = skew[0];
-        matrix = multiply(matrix, temp);
-      }
-
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          matrix[i][j] *= scale[i];
-        }
-      }
-
-      if (is2D(matrix)) {
-        return {
-          t: 'matrix',
-          d: [matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1],
-              matrix[3][0], matrix[3][1]]
-        };
-      }
-      return {
-        t: 'matrix3d',
-        d: matrix[0].concat(matrix[1], matrix[2], matrix[3])
-      };
-    }
-    return composeMatrix;
-  })();
-
-  var clamp = function(x, min, max) {
-    return Math.max(Math.min(x, max), min);
-  };
-
-  var isDefined = function(val) {
-    return typeof val !== 'undefined';
-  };
-
-  var isDefinedAndNotNull = function(val) {
-    return isDefined(val) && (val !== null);
-  };
-
-  var interp = function(from, to, f, type) {
-    if (Array.isArray(from) || Array.isArray(to)) {
-      return interpArray(from, to, f, type);
-    }
-    var zero = (type && type.indexOf('scale') === 0) ? 1 : 0;
-    to = isDefinedAndNotNull(to) ? to : zero;
-    from = isDefinedAndNotNull(from) ? from : zero;
-
-    return to * f + from * (1 - f);
-  };
-
-  var interpArray = function(from, to, f, type) {
-    WEB_ANIMATIONS_TESTING && console.assert(
-        Array.isArray(from) || from === null,
-        'From is not an array or null');
-    WEB_ANIMATIONS_TESTING && console.assert(
-        Array.isArray(to) || to === null,
-        'To is not an array or null');
-    WEB_ANIMATIONS_TESTING && console.assert(
-        from === null || to === null || from.length === to.length,
-        'Arrays differ in length ' + from + ' : ' + to);
-    var length = from ? from.length : to.length;
-
-    var result = [];
-    for (var i = 0; i < length; i++) {
-      result[i] = interp(from ? from[i] : null, to ? to[i] : null, f, type);
-    }
-    return result;
-  };
-
-  function interpolateDecomposedTransformsWithMatrices(fromM, toM, f) {
-    var fromMValue = fromM.d;
-    var toMValue = toM.d;
-    var product = scope.dot(fromMValue.quaternion, toMValue.quaternion);
-    product = clamp(product, -1.0, 1.0);
-
-    var quat = [];
-    if (product === 1.0) {
-      quat = fromMValue.quaternion;
-    } else {
-      var theta = Math.acos(product);
-      var w = Math.sin(f * theta) * 1 / Math.sqrt(1 - product * product);
-
-      for (var i = 0; i < 4; i++) {
-        quat.push(fromMValue.quaternion[i] * (Math.cos(f * theta) - product * w) +
-                  toMValue.quaternion[i] * w);
-      }
-    }
-
-    var translate = interp(fromMValue.translate, toMValue.translate, f);
-    var scale = interp(fromMValue.scale, toMValue.scale, f);
-    var skew = interp(fromMValue.skew, toMValue.skew, f);
-    var perspective = interp(fromMValue.perspective, toMValue.perspective, f);
-
-    return composeMatrix(translate, scale, skew, quat, perspective);
-  }
-
   function interpTransformValue(from, to, f) {
     var type = from.t ? from.t : to.t;
     switch (type) {
@@ -198,9 +34,13 @@
       case 'skew':
       case 'skewX':
       case 'skewY':
-        return {t: type, d: interp(from.d, to.d, f, type)};
+        return {t: type, d: scope.interp(from.d, to.d, f, type)};
       default:
         // Transforms with lengthType parameters.
+        console.log('from.d');
+        console.log(from.d);
+        console.log('to.d');
+        console.log(to.d);
         var result = [];
         var maxVal;
         if (from.d && to.d) {
@@ -213,14 +53,10 @@
         for (var j = 0; j < maxVal; j++) {
           var fromVal = from.d ? from.d[j] : {};
           var toVal = to.d ? to.d[j] : {};
-          result.push(interp(fromVal, toVal, f));
+          result.push(scope.interp(fromVal, toVal, f));
         }
         return {t: type, d: result};
     }
-  }
-
-  function isMatrix(item) {
-    return item.t[0] === 'm';
   }
 
   function interpolate(from, to, f) {
@@ -236,14 +72,15 @@
     }
 
     if (from.t && from.t == 'decomposedMatrix')
-      return interpolateDecomposedTransformsWithMatrices(from, to, f);
+      return scope.interpolateDecomposedTransformsWithMatrices(from, to, f);
 
     WEB_ANIMATIONS_TESTING && console.assert(
       Array.isArray(from) && Array.isArray(to),
       'If interpolation arguments are not numbers, bools or matrices they must be arrays');
 
     var isTransform = function(list) {
-      var transformFunctionNames = ('matrix|matrix3d|perspective|' +
+      // matrix and matrix3d omitted as they should be handled by matrix decomposition.
+      var transformFunctionNames = ('perspective|' +
         'rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|' +
         'scaley|scalez|scale3d|skew|skewx|skewy|translate|' +
         'translatex|translatey|translatez|translate3d').split('|');
@@ -251,10 +88,29 @@
     }
 
     if (from.length == to.length) {
-      var interpFunction = isTransform(from) ? interpTransformValue : interpolate;
+      console.log('from');
+      console.log(from);
+      console.log('to');
+      console.log(to);
+      // var interpFunction = isTransform(from) ? interpTransformValue : interpolate;
+      var interpFunction = interpolate;
       var out = [];
-      for (var i = 0; i < from.length; i++)
-        out.push(interpFunction(from[i], to[i], f));
+      // for (var i = 0; i < from.length; i++)
+      //   out.push(interpFunction(from[i], to[i], f));
+      // return out;
+      for (var i = 0; i < from.length; i++) {
+        var fromVal = from[i];
+        var toVal = to[i];
+        if (from[i].d) {
+          fromVal = from[i].d;
+          toVal = to[i].d;
+        }
+        console.log('fromVal');
+        console.log(fromVal);
+        console.log('toVal');
+        console.log(toVal);
+        out.push(interpFunction(fromVal, toVal, f));
+      }
       return out;
     }
     throw 'Mismatched interpolation arguments ' + from + ':' + to;
---
+++ src/matrix-interpolation.js
@@ -0,0 +1,169 @@
+(function(scope, testing) {
+  var composeMatrix = (function() {
+    function multiply(a, b) {
+      var result = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+          for (var k = 0; k < 4; k++) {
+            result[i][j] += b[i][k] * a[k][j];
+          }
+        }
+      }
+      return result;
+    }
+
+    function is2D(m) {
+      return (
+          m[0][2] == 0 &&
+          m[0][3] == 0 &&
+          m[1][2] == 0 &&
+          m[1][3] == 0 &&
+          m[2][0] == 0 &&
+          m[2][1] == 0 &&
+          m[2][2] == 1 &&
+          m[2][3] == 0 &&
+          m[3][2] == 0 &&
+          m[3][3] == 1);
+    }
+
+    function composeMatrix(translate, scale, skew, quat, perspective) {
+      var matrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+
+      for (var i = 0; i < 4; i++) {
+        matrix[i][3] = perspective[i];
+      }
+
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          matrix[3][i] += translate[j] * matrix[j][i];
+        }
+      }
+
+      var x = quat[0], y = quat[1], z = quat[2], w = quat[3];
+
+      var rotMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+
+      rotMatrix[0][0] = 1 - 2 * (y * y + z * z);
+      rotMatrix[0][1] = 2 * (x * y - z * w);
+      rotMatrix[0][2] = 2 * (x * z + y * w);
+      rotMatrix[1][0] = 2 * (x * y + z * w);
+      rotMatrix[1][1] = 1 - 2 * (x * x + z * z);
+      rotMatrix[1][2] = 2 * (y * z - x * w);
+      rotMatrix[2][0] = 2 * (x * z - y * w);
+      rotMatrix[2][1] = 2 * (y * z + x * w);
+      rotMatrix[2][2] = 1 - 2 * (x * x + y * y);
+
+      matrix = multiply(matrix, rotMatrix);
+
+      var temp = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+      if (skew[2]) {
+        temp[2][1] = skew[2];
+        matrix = multiply(matrix, temp);
+      }
+
+      if (skew[1]) {
+        temp[2][1] = 0;
+        temp[2][0] = skew[0];
+        matrix = multiply(matrix, temp);
+      }
+
+      if (skew[0]) {
+        temp[2][0] = 0;
+        temp[1][0] = skew[0];
+        matrix = multiply(matrix, temp);
+      }
+
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          matrix[i][j] *= scale[i];
+        }
+      }
+
+      if (is2D(matrix)) {
+        return {
+          t: 'matrix',
+          d: [matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1],
+              matrix[3][0], matrix[3][1]]
+        };
+      }
+      return {
+        t: 'matrix3d',
+        d: matrix[0].concat(matrix[1], matrix[2], matrix[3])
+      };
+    }
+    return composeMatrix;
+  })();
+
+  var isDefined = function(val) {
+    return typeof val !== 'undefined';
+  };
+
+  var isDefinedAndNotNull = function(val) {
+    return isDefined(val) && (val !== null);
+  };
+
+  var interp = function(from, to, f, type) {
+    if (Array.isArray(from) || Array.isArray(to)) {
+      return interpArray(from, to, f, type);
+    }
+    var zero = (type && type.indexOf('scale') === 0) ? 1 : 0;
+    to = isDefinedAndNotNull(to) ? to : zero;
+    from = isDefinedAndNotNull(from) ? from : zero;
+
+    return to * f + from * (1 - f);
+  };
+
+  var interpArray = function(from, to, f, type) {
+    WEB_ANIMATIONS_TESTING && console.assert(
+        Array.isArray(from) || from === null,
+        'From is not an array or null');
+    WEB_ANIMATIONS_TESTING && console.assert(
+        Array.isArray(to) || to === null,
+        'To is not an array or null');
+    WEB_ANIMATIONS_TESTING && console.assert(
+        from === null || to === null || from.length === to.length,
+        'Arrays differ in length ' + from + ' : ' + to);
+    var length = from ? from.length : to.length;
+
+    var result = [];
+    for (var i = 0; i < length; i++) {
+      result[i] = interp(from ? from[i] : null, to ? to[i] : null, f, type);
+    }
+    return result;
+  };
+
+  var clamp = function(x, min, max) {
+    return Math.max(Math.min(x, max), min);
+  };
+
+  function interpolateDecomposedTransformsWithMatrices(fromM, toM, f) {
+    var fromMValue = fromM.d;
+    var toMValue = toM.d;
+    var product = scope.dot(fromMValue.quaternion, toMValue.quaternion);
+    product = clamp(product, -1.0, 1.0);
+
+    var quat = [];
+    if (product === 1.0) {
+      quat = fromMValue.quaternion;
+    } else {
+      var theta = Math.acos(product);
+      var w = Math.sin(f * theta) * 1 / Math.sqrt(1 - product * product);
+
+      for (var i = 0; i < 4; i++) {
+        quat.push(fromMValue.quaternion[i] * (Math.cos(f * theta) - product * w) +
+                  toMValue.quaternion[i] * w);
+      }
+    }
+
+    var translate = interp(fromMValue.translate, toMValue.translate, f);
+    var scale = interp(fromMValue.scale, toMValue.scale, f);
+    var skew = interp(fromMValue.skew, toMValue.skew, f);
+    var perspective = interp(fromMValue.perspective, toMValue.perspective, f);
+
+    return composeMatrix(translate, scale, skew, quat, perspective);
+  }
+
+  scope.interpolateDecomposedTransformsWithMatrices = interpolateDecomposedTransformsWithMatrices;
+  scope.interp = interp;
+
+})(webAnimationsMinifill, webAnimationsTesting);
