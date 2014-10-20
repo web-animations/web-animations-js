3413f7718e29ef531e9336bfa944e682a3483dea
Shane Stephens <shanestephens@google.com>
2014-10-20 14:48:18 +1100

    Working build definitions for minimized minifill and maxifill.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,36 +14,46 @@ module.exports = function(grunt) {
   var config = {
     uglify: {},
     template: {},
-    wrap: {}
+    wrap: {},
+    sourceMapConcat: {},
   };
 
-  function uglify(sources, target, options) {
+  function concat(sources, target, defines) {
     config.uglify[target] = {
       options: {
         sourceMap: true,
         sourceMapName: target + '.map',
-        banner: grunt.file.read('templates/boilerplate'),
-        wrap: true,
+        wrap: false,
         compress: {
-          global_defs: options,
-          dead_code: true
+          global_defs: defines,
+          dead_code: false
         },
-        mangle: {
-          eval: true
-        },
+        mangle: false
       },
       nonull: true,
       dest: target,
       src: sources
     };
-    return target;
+    return 'uglify:' + target;
+  }
+
+  function compress(source, target, defines) {
+    var name = concat([source], target, defines);
+    var record = config.uglify[target];
+    record.options.sourceMapIn = source + '.map';
+    record.options.banner = grunt.file.read('templates/boilerplate');
+    record.options.wrap = true;
+    record.options.compress.dead_code = true;
+    record.options.mangle = { eval: true };
+    return name;
   }
 
   function gen(sources, target) {
     var suffix = target == targetConfig.defaultTarget ? '' : '-' + target
-    generateFromTemplate('templates/web-animations.js', {target: target}, 'web-animations' + suffix + '.js');
-    generateFromTemplate('templates/web-animations.html', {src: sources}, 'web-animations' + suffix + '.html');
-    generateFromTemplate('templates/runner.html', {target: target}, 'test/runner' + suffix + '.html');
+    var newGens = [generateFromTemplate('templates/web-animations.js', {target: target}, 'web-animations' + suffix + '.js'),
+      generateFromTemplate('templates/web-animations.html', {src: sources}, 'web-animations' + suffix + '.html'),
+      generateFromTemplate('templates/runner.html', {target: target}, 'test/runner' + suffix + '.html')];
+    return newGens;
   }
 
   function generateFromTemplate(source, data, target) {
@@ -55,29 +65,62 @@ module.exports = function(grunt) {
       },
       files: targetSpec
     }
-    return target;
+    return 'template:' + target;
   }
 
   function guard(source, target) {
     config.wrap[target] = {
       source: source,
-      preamble: '(function() { if (false) { return; } ',
+      preamble: '(function() {\n' +
+                '  var player = document.body.animate([], 0);\n' +
+                '  var load = true;\n' +
+                '  if (player !== undefined) {\n' +
+                '    load = false;\n' +
+                '    "play|currentTime|pause|reverse|playbackRate|cancel|finish|startTime|playState".split("|").forEach(function(t) {\n' +
+                '      if (player[t] === undefined) {\n' +
+                '        load = true;\n' +
+                '      }\n' +
+                '    });\n' +
+                '  }\n' +
+                '  if (!load) { return; }',
       postamble: '})();'
     };
+    return 'wrap:' + target;
   }
 
-  var uglifyConfig = {
+  function concatWithMaps(sources, target) {
+    config.sourceMapConcat[target] = {
+      sources: sources
+    }
+    return 'sourceMapConcat:' + target;
+  };
+
+  var concatDefines = {
     WEB_ANIMATIONS_TESTING: false
   };
 
-  uglify(targetConfig.minifill.src, 'web-animations-minifill.min.js', uglifyConfig);
-  uglify(targetConfig.maxifill.src, 'web-animations.min.js', uglifyConfig);
+  // build the minifill
+  grunt.registerTask('minifill.min',
+      [
+        concat(targetConfig.scopeSrc.concat(targetConfig.sharedSrc).concat(targetConfig.minifillSrc), 'inter-raw-minifill.js', concatDefines),
+        guard('inter-raw-minifill.js', 'inter-web-animations-minifill.js'),
+        compress('inter-web-animations-minifill.js', 'web-animations-minifill.min.js', concatDefines)
+      ]);
 
-  uglify(targetConfig.minifillSrc, 'component-minifill.js', uglifyConfig);
-  guard('component-minifill.js', 'guarded-component-minifill.js');
+  // build the maxifill
+  grunt.registerTask('maxifill.min',
+      [
+        concat(targetConfig.scopeSrc.concat(targetConfig.sharedSrc), 'inter-maxifill-preamble.js', concatDefines),
+        concat(targetConfig.minifillSrc, 'inter-component-minifill.js', concatDefines),
+        guard('inter-component-minifill.js', 'inter-guarded-component-minifill.js'),
+        concat(targetConfig.maxifillSrc, 'inter-component-maxifill.js', concatDefines),
+        concatWithMaps(['inter-maxifill-preamble.js', 'inter-guarded-component-minifill.js', 'inter-component-maxifill.js'],
+            'inter-web-animations-maxifill.js'),
+        compress('inter-web-animations-maxifill.js', 'web-animations.min.js', concatDefines)
+      ]);
 
-  gen(targetConfig.minifill.src, 'minifill');
-  gen(targetConfig.maxifill.src, 'maxifill');
+  grunt.registerTask('minifill', gen(targetConfig.minifill.src, 'minifill'));
+  grunt.registerTask('maxifill', gen(targetConfig.maxifill.src, 'maxifill'));
 
   var testTargets = {'minifill': {}, 'maxifill': {}};
 
@@ -85,6 +128,7 @@ module.exports = function(grunt) {
     uglify: config.uglify,
     template: config.template,
     wrap: config.wrap,
+    sourceMapConcat: config.sourceMapConcat,
     checkrepo: {
       all: {
         clean: true,
@@ -143,30 +187,58 @@ module.exports = function(grunt) {
     });
   });
 
+  grunt.task.registerMultiTask('sourceMapConcat', 'concat source files and produce combined source map',
+    function() {
+      var sources = this.data.sources.map(grunt.file.read);
+      var sourceMaps = this.data.sources.map(function(f) { return grunt.file.read(f + '.map'); });
+      var out = "";
+      var outMapGenerator = new sourceMap.SourceMapGenerator({file: this.target});
+      var lineDelta = 0;
+      for (var i = 0; i < sources.length; i++) {
+        out += sources[i];
+        new sourceMap.SourceMapConsumer(sourceMaps[i]).eachMapping(function(mapping) {
+          outMapGenerator.addMapping({
+            generated: {line: mapping.generatedLine + lineDelta, column: mapping.generatedColumn},
+            original: {line: mapping.originalLine, column: mapping.originalColumn},
+            source: mapping.source, name: mapping.name});
+        });
+        var sourceLines = sources[i].split('\n');
+        lineDelta += sourceLines.length;
+        if (sources[i][sources[i].length - 1] !== '\n') {
+          out += '\n';
+        }
+      }
+      grunt.file.write(this.target, out);
+      grunt.file.write(this.target + '.map', outMapGenerator.toString());
+    });
+
   grunt.task.registerMultiTask('wrap', 'Wrap <target> source file and update source map',
     function() {
       var inFile = grunt.file.read(this.data.source);
       var inMap = grunt.file.read(this.data.source + '.map');
       var inLines = inFile.split('\n');
       var i = 0;
-      
+
       // Discover copyright header
       while (inLines[i].length < 2 || inLines[i].substring(0, 2) == '//') {
         i++;
       }
-     
+
       // Fix mapping footer
       var postamble = this.data.postamble;
       if (inLines[inLines.length - 1].substring(0, 21) == '//# sourceMappingURL=') {
         postamble += '\n//# sourceMappingURL=' + this.target + '.map';
       }
 
-      grunt.file.write(
-          this.target, inLines.slice(0, i).join('\n') + 
-          this.data.preamble + 
-          inLines.slice(i, inLines.length - 1).join('\n') + 
-          postamble
-      );
+      if (i > 0) {
+        var banner = inLines.slice(0, i).join('\n') + '\n';
+      } else {
+        var banner = '';
+      }
+
+      var source = inLines.slice(i, inLines.length - 1).join('\n');
+
+      grunt.file.write(this.target, banner + this.data.preamble + source + postamble);
       var preLines = this.data.preamble.split('\n');
       var lineDelta = preLines.length;
       if (this.data.preamble[this.data.preamble.length - 1] == '\n') {
@@ -175,7 +247,6 @@ module.exports = function(grunt) {
         var charDelta = preLines[lineDelta - 1].length;
         lineDelta -= 1;
       }
-      console.log(lineDelta, charDelta);
       var inMapConsumer = new sourceMap.SourceMapConsumer(inMap);
       var outMapGenerator = new sourceMap.SourceMapGenerator({file: this.target});
       inMapConsumer.eachMapping(function(mapping) {
@@ -192,11 +263,11 @@ module.exports = function(grunt) {
     });
 
   grunt.task.registerTask('clean', 'Remove files generated by grunt', function() {
-    grunt.file.expand('web-animations-*').concat(grunt.file.expand('test/runner-*.html')).forEach(function(file) {
+    grunt.file.expand('web-animations*').concat(grunt.file.expand('test/runner-*.html')).concat(grunt.file.expand('inter-*')).forEach(function(file) {
       grunt.file.delete(file);
       grunt.log.writeln('File ' + file + ' removed');
     });
   });
 
-  grunt.task.registerTask('default', ['uglify', 'template', 'wrap', 'gjslint']);
+  grunt.task.registerTask('default', ['minifill', 'maxifill', 'minifill.min', 'maxifill.min', 'gjslint']);
 };
