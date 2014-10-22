7e36deec66e76905a4fe5c1ff74aa64c9398de0a
Douglas Stockwell <dstockwell@chromium.org>
2014-10-22 17:55:39 +1100

    Add a simple testharness runner using mocha
    There's probably some way to do this using a karma plugin directly, but
    this works well enough for now.

---
+++ test/testharness-tests.js
@@ -0,0 +1,14 @@
+// FIXME: Generate this file.
+var testHarnessTests = [
+  'blink/simple-keyframes.html',
+  'blink/add-keyframes.html',
+  'blink/2-keyframes-with-offsets.html',
+  'blink/3-keyframes-with-offsets.html',
+  'blink/eased-keyframes.html',
+  'blink/keyframes-with-null-offsets.html',
+  'blink/getAnimationPlayers.html',
+  'blink/insufficient-keyframes.html',
+  'blink/keyframe-properties.html',
+  'blink/out-of-order-keyframes.html',
+  'blink/same-offset-keyframes.html',
+];
