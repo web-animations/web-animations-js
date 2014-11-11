8a8b521bfb34a28835333a24b2782bc9d626ead0
Douglas Stockwell <dstockwell@chromium.org>
2014-11-11 11:20:49 +1100

    Dont require minification for testharness tests

--- target-loader.js
+++ target-loader.js
@@ -5,25 +5,9 @@
 
   // Native implementation detection.
 
-  var load = true;
-  if (document.documentElement.animate) {
-    var player = document.documentElement.animate([], 0);
-
-    if (player) {
-      load = false;
-      "play|currentTime|pause|reverse|playbackRate|cancel|finish|startTime|playState".split('|').forEach(function(t) {
-        if (player[t] === undefined) {
-          load = true;
-        }
-      });
-    }
-  }
-
-  if (load) {
-    var scripts = document.getElementsByTagName('script');
-    var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
-    webAnimationsTargetConfig[target].src.forEach(function(sourceFile) {
-      document.write('<script src="' + location + sourceFile + '"></script>');
-    });
-  }
+  var scripts = document.getElementsByTagName('script');
+  var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
+  webAnimationsTargetConfig[target].src.forEach(function(sourceFile) {
+    document.write('<script src="' + location + sourceFile + '"></script>');
+  });
 })();
