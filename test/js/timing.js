b9767f4bdfad4c70900508fbbc726b87604a0b8d
Sam Thorogood <sam.thorogood@gmail.com>
2014-11-13 13:01:20 +1100

    more timing tests

--- test/js/timing.js
+++ test/js/timing.js
@@ -24,4 +24,47 @@ suite('timing-tests', function() {
     tick(850);
     assert.equal(player.currentTime, 500);
   });
+
+  test('immediate pause iteration', function() {
+    var animation = new Animation(document.body, [], { duration: 1000 });
+    var player = document.timeline.play(animation);
+    player.pause();
+
+    tick(50);
+    assert(!player.currentTime);
+    assert(!player.startTime);
+
+    tick(150);
+    assert(!player.currentTime);
+    assert(!player.startTime);
+
+    player.play();
+
+    tick(250);
+    assert.equal(player.currentTime, 100);
+    assert.equal(player.startTime, 150);
+  });
+
+  test('pause and scrub', function() {
+    var target = document.createElement('div');
+    document.body.appendChild(target);
+
+    var animation = new Animation(target, [
+      { background: 'blue' },
+      { background: 'red' }
+    ], { duration: 1000 });
+    var player = document.timeline.play(animation);
+    tick(100);
+    player.pause();
+
+    player.currentTime = 200;
+    // http://www.w3.org/TR/web-animations/#the-current-time-of-a-player
+    // currentTime should now mean 'hold time'?
+    assert.equal(player.currentTime, 500);
+    player.play();
+
+    tick(200);
+    assert.equal(player.currentTime, 300);
+    assert.equal(player.startTime, -100);
+  });
 });
