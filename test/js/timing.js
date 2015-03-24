8d3f8773b740a615526d1c071235ed56c49c26bb
rjwright <rjwright@chromium.org>
2015-03-24 11:16:38 +1100

    rename for timing.js. Remove setup for this suite as it is not needed.

--- test/js/timing.js
+++ test/js/timing.js
@@ -1,51 +1,47 @@
 suite('timing', function() {
-  setup(function() {
-    webAnimations1.timeline._players = [];
-  });
-
   test('pause and scrub', function() {
-    var player = document.body.animate([], { duration: 1000 });
-    player.pause();
+    var animation = document.body.animate([], { duration: 1000 });
+    animation.pause();
 
-    player.currentTime = 500;
-    assert.equal(player.currentTime, 500);
+    animation.currentTime = 500;
+    assert.equal(animation.currentTime, 500);
   });
 
   test('pause, scrub and play', function() {
     var target = document.createElement('div');
     document.body.appendChild(target);
 
-    var player = target.animate([
+    var animation = target.animate([
       { background: 'blue' },
       { background: 'red' }
     ], { duration: 1000 });
     tick(100);
-    player.pause();
+    animation.pause();
 
-    player.currentTime = 200;
-    // http://www.w3.org/TR/web-animations/#the-current-time-of-a-player
+    animation.currentTime = 200;
+    // http://www.w3.org/TR/web-animations/#the-current-time-of-an-animation
     // currentTime should now mean 'hold time' - this allows scrubbing.
-    assert.equal(player.currentTime, 200);
-    player.play();
+    assert.equal(animation.currentTime, 200);
+    animation.play();
 
     tick(200);
     tick(300);
-    assert.equal(player.currentTime, 300);
-    assert.equal(player.startTime, 0);
+    assert.equal(animation.currentTime, 300);
+    assert.equal(animation.startTime, 0);
   });
 
   test('sanity-check NaN timing', function() {
     // This has no actual tests, but will infinite loop without fix.
 
-    var player = document.body.animate([], {
+    var animation = document.body.animate([], {
       duration: 2000,
       easing: 'ease-in'  // fails only with cubic easing, not linear
     });
     tick(100);
-    player.currentTime = NaN;
+    animation.currentTime = NaN;
     tick(200);
 
-    player = document.body.animate([], { duration: NaN, easing: 'ease-out' });
+    animation = document.body.animate([], { duration: NaN, easing: 'ease-out' });
     tick(300);
   });
 
