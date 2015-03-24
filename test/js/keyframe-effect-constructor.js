44f1e5203de05af89c7002bd7d3d9640373b1cf3
rjwright <rjwright@chromium.org>
2015-03-24 11:11:42 +1100

    Rename for keyframe-effect-constructors.js, matrix-interpolation.js

--- test/js/keyframe-effect-constructor.js
+++ test/js/keyframe-effect-constructor.js
@@ -1,15 +1,15 @@
-suite('animation-constructor', function() {
+suite('keyframe-effect-constructor', function() {
   setup(function() {
-    document.timeline.getAnimations().forEach(function(player) {
-      player.cancel();
+    document.timeline.getAnimations().forEach(function(animation) {
+      animation.cancel();
     });
   });
 
-  test('Playing an Animation makes a Player', function() {
-    var animation = new Animation(document.body, [], 1000);
+  test('Playing a KeyframeEffect makes an Animation', function() {
+    var keyframeEffect = new KeyframeEffect(document.body, [], 1000);
     assert.equal(document.body.getAnimations().length, 0);
 
-    var player = document.timeline.play(animation);
+    var animation = document.timeline.play(keyframeEffect);
     tick(200);
     assert.equal(document.body.getAnimations().length, 1);
 
@@ -17,7 +17,7 @@ suite('animation-constructor', function() {
     assert.equal(document.body.getAnimations().length, 0);
   });
 
-  test('Setting the timing function on an Animation works', function() {
+  test('Setting the timing function on a KeyframeEffect works', function() {
     function leftAsNumber(target) {
       left = getComputedStyle(target).left;
       return Number(left.substring(0, left.length - 2));
@@ -30,11 +30,11 @@ suite('animation-constructor', function() {
     document.body.appendChild(target1);
     document.body.appendChild(target2);
 
-    var animation1 = new Animation(target1, [{left: '0px'}, {left: '50px'}], 1000);
-    var animation2 = new Animation(target2, [{left: '0px'}, {left: '50px'}], {duration: 1000, easing: 'ease-in'});
+    var keyframeEffect1 = new KeyframeEffect(target1, [{left: '0px'}, {left: '50px'}], 1000);
+    var keyframeEffect2 = new KeyframeEffect(target2, [{left: '0px'}, {left: '50px'}], {duration: 1000, easing: 'ease-in'});
 
-    var player1 = document.timeline.play(animation1);
-    var player2 = document.timeline.play(animation2);
+    var animation1 = document.timeline.play(keyframeEffect1);
+    var animation2 = document.timeline.play(keyframeEffect2);
 
     tick(0);
     assert.equal(leftAsNumber(target1), 0);
@@ -49,32 +49,32 @@ suite('animation-constructor', function() {
     assert.closeTo(leftAsNumber(target2), 15.25, 1);
   });
 
-  test('Timing is always converted to AnimationTimingInput', function() {
+  test('Timing is always converted to an AnimationEffectTiming', function() {
     var target = document.createElement('div');
     document.body.appendChild(target);
 
     var keyframes = [{background: 'blue'}, {background: 'red'}];
 
-    var animation = new Animation(target, keyframes, 200);
-    assert.equal(animation.timing.duration, 200);
+    var keyframeEffect = new KeyframeEffect(target, keyframes, 200);
+    assert.equal(keyframeEffect.timing.duration, 200);
 
-    animation = new Animation(target, keyframes);
-    assert.isDefined(animation.timing);
+    keyframeEffect = new KeyframeEffect(target, keyframes);
+    assert.isDefined(keyframeEffect.timing);
 
-    animation = new Animation(target, keyframes, {duration: 200});
-    var group = new AnimationGroup([animation]);
+    keyframeEffect = new KeyframeEffect(target, keyframes, {duration: 200});
+    var group = new GroupEffect([keyframeEffect]);
     assert.equal(group.timing.duration, 'auto');
   });
 
-  test('Handle null target on Animation', function() {
-    var animation = new Animation(null, function(tf) {
+  test('Handle null target for KeyframeEffect', function() {
+    var keyframeEffect = new KeyframeEffect(null, function(tf) {
       // noop
     }, 200);
 
-    var player = document.timeline.play(animation);
-    assert.isNotNull(player);
+    var animation = document.timeline.play(keyframeEffect);
+    assert.isNotNull(animation);
     tick(50);
     tick(150);
-    assert.equal(player.currentTime, 100);
+    assert.equal(animation.currentTime, 100);
   });
 });
